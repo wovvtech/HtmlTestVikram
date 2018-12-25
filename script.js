22af7878a7749d91dc69ec1b7e4985392fc4fa27
wovv 004 <wovv004@wovvipos.com>
2018-12-25 17:19:46 +0530

    scrolling fix

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-window.smoothScroll = function(target) {
-    var scrollContainer = target;
-    do { 
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
-    var targetY = 0;
-    do { 
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
-
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+var pageHeight = window.innerHeight;
+    var isAnimating = false;
+    document.body.style.transform = 'translate3d(0px,0px,0px)';
+    
+    document.addEventListener('scroll', function(e){
+        document.body.scrollTop = 0;
+    });
+    document.addEventListener('wheel', wheelListener);
+    
+    function wheelListener(e) {
+        if(e.deltaY > 0) {
+        scrollPage(-pageHeight);
+        } else {
+        scrollPage(+pageHeight);
+        }
     }
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
+    function SinglescrollPage(){
+        scrollPage(-pageHeight);
+    }
+    function scrollPage(scrollSize) {
+        if(isAnimating){
+            return;
+        }
+        isAnimating = true;
+        var yPos = getNewYPos(scrollSize);
+        
+        document.body.style.transform = 'translate3d(0px,'+ yPos + ',0px)';
+    }
+    function getNewYPos(add){
+        var oldYPos = document.body.style.transform.split(',')[1];
+        oldYPos = parseInt(oldYPos.replace(/px/,''));
+        var newYPos = oldYPos + add;
+        if(newYPos > 0){
+            isAnimating = false;
+        }
+        return Math.min(0, newYPos) + 'px';
+    }
+    document.body.addEventListener('transitionend', function(){
+        setTimeout(function(){ isAnimating = false; }, 500);
+        document.addEventListener('wheel', wheelListener);
+    });
+
